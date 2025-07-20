d51d5798bc7875a40fba9362d4cc31702214270e
schappellshow <mschappe@mac.com>
2025-07-20 09:38:33 -0400

    updated install script

--- install_test_1.sh
+++ install_test_1.sh
@@ -2,7 +2,8 @@
 
 # This is my attempt at an install script to facilitate faster setup of all of my OM stuff
 
-
+# Update System
 sudo dnf clean all;dnf clean all; sudo dnf distro-sync --allowerasing --refresh -y
 
-sudo dnf install ghostty
+# Install applications
+sudo dnf install ghostty kitty 
