4ae491ac005634e225fc61ec01072ea67579bd9b
schappellshow <mschappe@mac.com>
2025-07-27 19:31:42 -0400

    added Pictures/Logo/ directory w/ images & Wallpapers directory w/ images

--- .conky/conky-startup.sh
+++ .conky/conky-startup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-if [ "$DESKTOP_SESSION" = "plasmax11" ]; then 
-   sleep 20s
-   killall conky
-   cd "$HOME/.conky"
-   conky -c "$HOME/.conky/titus_desktop.conkyrc" &
-   exit 0
-fi
 if [ "$DESKTOP_SESSION" = "plasma" ]; then 
    sleep 20s
    killall conky
    cd "$HOME/.conky"
    conky -c "$HOME/.conky/titus_modified.conkyrc" &
+   exit 0
+fi
+if [ "$DESKTOP_SESSION" = "plasmax11" ]; then 
+   sleep 20s
+   killall conky
+   cd "$HOME/.conky"
+   conky -c "$HOME/.conky/titus_desktop.conkyrc" &
    exit 0
 fi
